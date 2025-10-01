b2803c29cce7269ed4890289e6800ee2ae36ce8c
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-10-01 15:29:39 +0530

    update type for other games too

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -1,12 +1,19 @@
-export interface TeamRow {
+export interface Row {
   team: string;
   played: number;
   won: number;
+  draw: number;
   loss: number;
   points: number;
+  goals_scored: number;
+  goal_difference: number;
+  cards?: {
+    yellow: number;
+    red: number;
+  };
 }
 
 export interface Game {
   name: string;
-  rows: TeamRow[];
+  rows: Row[];
 }
