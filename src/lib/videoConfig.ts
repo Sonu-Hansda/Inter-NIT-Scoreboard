e6dfd4e4d368dde4f51b8431a8d8e1ed7b82a5a6
Sonu-Hansda <sonukumarhansda61@gmail.com>
2025-10-04 14:01:17 +0530

    update futsal video stream

--- src/lib/videoConfig.ts
+++ src/lib/videoConfig.ts
@@ -12,7 +12,6 @@ export interface SportVideos {
 
 export const VIDEO_CONFIG: SportVideos = {
   football: [
-    { label: 'Inogration', url: 'https://www.youtube.com/embed/NpdMipd_ums?si=SWiqSxmde-LF7yAL' },
     { label: 'Football Ground 1', url: 'https://www.youtube.com/embed/AGBY6b3z7h4?si=4KUMCma6-guchQGN' },
     { label: 'Football Ground 4', url: 'https://www.youtube.com/embed/_p2Sui8CoV4?si=2E3bAwvPvp-cC4mk' },
   ],
@@ -25,6 +24,7 @@ export const VIDEO_CONFIG: SportVideos = {
     // { label: 'View 2', url: 'https://www.youtube.com/embed/VIDEO_ID_TT_GIRLS_2' },
   ],
   futsal: [
+    { label: 'Footbal Ground 2', url: 'https://www.youtube.com/embed/NpdMipd_ums?si=SWiqSxmde-LF7yAL' },
     // { label: 'View 1', url: 'https://www.youtube.com/embed/VIDEO_ID_FUTSAL_1' },
     // { label: 'View 2', url: 'https://www.youtube.com/embed/VIDEO_ID_FUTSAL_2' },
   ],
